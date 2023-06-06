56bff6dd7eba57f199a0a38a6953e2ab9f3ff51d
dmkwis <dw418484@students.mimuw.edu.pl>
2023-06-07 00:44:57 +0200

    Fix positional encoding.

--- model/BERT.py
+++ model/BERT.py
@@ -5,22 +5,19 @@ from dataclasses import dataclass
 import numpy as np
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, hidden_size, dropout_rate, max_length):
+    def __init__(self, hidden_size, max_length):
         super(PositionalEncoding, self).__init__()
-
-        self.dropout = nn.Dropout(dropout_rate)
 
         position = torch.arange(0, max_length).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, hidden_size, 2) * -(np.log(10000.0) / hidden_size))
         pe = torch.zeros(max_length, hidden_size)
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
+        pe = pe.unsqueeze(0)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
-        x = x + self.pe[:x.size(0), :]
-        return self.dropout(x)
+        return x + self.pe
     
 
 @dataclass
@@ -39,7 +36,7 @@ class BERT(nn.Module):
         super(BERT, self).__init__()
 
         self.embedding = nn.Embedding(config.vocab_size, config.hidden_size)
-        self.positional_encoding = PositionalEncoding(config.hidden_size, config.dropout, config.max_seq_length)
+        self.positional_encoding = PositionalEncoding(config.hidden_size, config.max_seq_length)
 
         encoder_layers = TransformerEncoderLayer(config.hidden_size, config.num_heads, config.ff_dim, config.dropout, batch_first=True)
         self.transformer_encoder = TransformerEncoder(encoder_layers, config.num_layers)
