c35cdb4fc3ee5f40f1822cc700a72078973e27c7
kar655 <karol123zag@gmail.com>
2023-06-18 19:10:06 +0200

    Add train loop template

---
+++ train.py
@@ -0,0 +1,106 @@
+import time
+import argparse
+from typing import Dict
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import pytorch_lightning as pl
+
+from dataset import generate_dataset, get_vocab_size
+from model.config import ModelConfig
+from model.transformer import TransformerEncoderDecoder
+
+
+class TrainingModule(pl.LightningModule):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.save_hyperparameters()
+
+        config = ModelConfig(
+            self.hparams.vocab_size,
+            self.hparams.hidden_size,
+            self.hparams.num_layers,
+            self.hparams.num_heads,
+            self.hparams.ff_dim,
+            self.hparams.dropout,
+            self.hparams.max_seq_length,
+        )
+        self.transformer = TransformerEncoderDecoder(config)
+
+    def training_step(self, batch, batch_idx: int):
+        x, y = batch
+
+        output = self.transformer(x, y)
+
+        return output
+
+    def validation_step(self, batch, batch_idx: int):
+        pass
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(
+            self.transformer.parameters(),
+            lr=self.hparams.lr,
+        )
+
+        return optimizer
+
+
+def parse_arguments() -> Dict[str, int]:
+    parser = argparse.ArgumentParser()
+    # Model params
+    parser.add_argument('--hidden_size', type=int, default=2,
+                        help='size of word embeddings')
+    parser.add_argument('--num_layers', type=int, default=2,
+                        help='number of layers')
+    parser.add_argument('--num_heads', type=int, default=2,
+                        help='number of heads per layer')
+    parser.add_argument('--ff_dim', type=int, default=2,
+                        help='size of embeddings in feed forward layer')
+    parser.add_argument('--dropout', type=float, default=0.1,
+                        help='dropout applied to layers (0 = no dropout)')
+
+    # Training params
+    parser.add_argument('--epochs', type=int, default=2,
+                        help='upper epoch limit')
+    parser.add_argument('--batch_size', type=int, default=2,
+                        help='batch size')
+    parser.add_argument('--lr', type=float, default=0.01,
+                        help='initial learning rate')
+    parser.add_argument('--seed', type=int, default=None,
+                        help='random seed')
+    parser.add_argument('--cuda', action='store_true', default=False,
+                        help='use CUDA')
+    parser.add_argument('--save', type=str, default='model.pt',
+                        help='path to save the final model')
+
+    args = parser.parse_args()
+    args = vars(args)
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    # TODO
+    args['vocab_size'] = get_vocab_size()
+    args['max_seq_length'] = 568
+    print('Using args:', args)
+
+    model = TrainingModule(**args)
+    trainer = pl.Trainer(
+        max_epochs=args['epochs'],
+        # accelerator='gpu',
+        # devices=1,
+    )
+
+    # train_dataloader, test_dataloader = generate_dataset()
+    train_dataloader = generate_dataset(args['batch_size'])
+    print('Generated dataset')
+
+    trainer.fit(
+        model,
+        train_dataloader,
+        # test_dataloader,
+    )
