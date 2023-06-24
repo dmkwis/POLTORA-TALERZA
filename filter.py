bf10f364617045ca374824060f25e19fde3fc9ae
woocash2 <59764862+woocash2@users.noreply.github.com>
2023-06-24 10:50:15 +0200

    Reduce vocabulary size and make data improvements (#11)

--- filter.py
+++ filter.py
@@ -1,9 +1,28 @@
 import dataset
 
-REMOVE_FRACTION = 0.5
+FINETUNE_SIZE = 100000
+PRETRAIN_SIZE = 300000
 
 
-def filter_rare(data_x, data_y):
+def filter_unique_word(data_x, data_y, word_cnt):
+    new_data_x, new_data_y = [], []
+    reduced = 0
+    for i, y in enumerate(data_y):
+        keep = True
+        for w in y:
+            if word_cnt[w] < 50:
+                keep = False
+                reduced += 1
+                break
+        if keep:
+            new_data_x.append(data_x[i])
+            new_data_y.append(y)
+
+    print('reduced words by:', reduced)
+    return new_data_x, new_data_y
+
+
+def filter_rare(data_x, data_y, word_cnt, data_size):
     data_scores = []
     for y in data_y:
         score = 0
@@ -17,25 +36,40 @@ def filter_rare(data_x, data_y):
     data_with_scores = sorted(data_with_scores, key = lambda a: a[1])
     new_data = list(zip(*data_with_scores))
     new_data = list(new_data[0])
-    new_data = new_data[int(REMOVE_FRACTION * len(new_data)):]
+    new_data = new_data[(-data_size):]
     new_data = list(zip(*new_data))
     new_data_x, new_data_y = list(new_data[0]), list(new_data[1])
+
+    print('reduced some more')
     return new_data_x, new_data_y
+
+
+def fill_word_cnt():
+    word_cnt = {}
+    for verse in pretrain_y + finetune_y:
+        words = set(verse)
+        for w in words:
+            if w not in word_cnt:
+                word_cnt[w] = 0
+            word_cnt[w] += 1
+    return word_cnt
 
 
 if __name__ == '__main__':
     pretrain_x, pretrain_y = dataset.get_data('pretrain')
     finetune_x, finetune_y = dataset.get_data('finetune')
 
-    word_cnt = {}
-    for verse in pretrain_y + finetune_y:
-        for w in verse:
-            if w not in word_cnt:
-                word_cnt[w] = 0
-            word_cnt[w] += 1
+    word_cnt = fill_word_cnt()
     
     print('words counted')
-    pretrain_x, pretrain_y = filter_rare(pretrain_x, pretrain_y)
+    pretrain_x, pretrain_y = filter_unique_word(pretrain_x, pretrain_y, word_cnt)
+    finetune_x, finetune_y = filter_unique_word(finetune_x, finetune_y, word_cnt)
+
+    word_cnt = fill_word_cnt()
+
+    pretrain_x, pretrain_y = filter_rare(pretrain_x, pretrain_y, word_cnt, PRETRAIN_SIZE)
+    finetune_x, finetune_y = filter_rare(finetune_x, finetune_y, word_cnt, FINETUNE_SIZE)
+
     with open('data/pretrain_x.txt', 'w') as file:
         pretrain_x = [' '.join(l) for l in pretrain_x]
         pretrain_x = [v.replace('\n ', '\n') for v in pretrain_x]
@@ -48,7 +82,6 @@ if __name__ == '__main__':
         file.write(pretrain_y)
     print('overwritten pretrain')
 
-    finetune_x, finetune_y = filter_rare(finetune_x, finetune_y)
     with open('data/finetune_x.txt', 'w') as file:
         finetune_x = [' '.join(l) for l in finetune_x]
         finetune_x = [v.replace('\n ', '\n') for v in finetune_x]
