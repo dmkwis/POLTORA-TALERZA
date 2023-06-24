50013c9c6bef9db66d1e09c23de5b9e328192185
woocash2 <lukasgniecki@gmail.com>
2023-06-24 11:44:44 +0200

    Proper inference

--- infer.py
+++ infer.py
@@ -1,5 +1,5 @@
 import argparse
-from dataset import w2i, i2w, PAD_TOKEN, START_TOKEN, END_TOKEN
+from dataset import w2i, i2w, PAD_TOKEN, START_TOKEN, END_TOKEN, NEWLINE_TOKEN
 import dataset
 import torch
 from model.transformer import TransformerEncoderDecoder
@@ -10,12 +10,15 @@ from train import TrainingModule
 # take the sequence of outputs from the transformer and convert it into a sentence
 def translate_output(transformer_outputs: torch.tensor) -> str:
     result = ''
-    _, n, l = transformer_outputs.shape
-    for i1 in range(n):
-        i = torch.argmax(transformer_outputs[0, i1, :])
-        word = i2w[i.item()]
-        print(i1, word)
-        if word != START_TOKEN and word != END_TOKEN and word != PAD_TOKEN:
+    for token in transformer_outputs:
+        word = i2w[token.item()]
+        if word == END_TOKEN:
+            break
+        if word == START_TOKEN or word == PAD_TOKEN:
+            continue
+        if word == NEWLINE_TOKEN:
+            result += word
+        else:
             result += word + ' '
     return result
 
@@ -23,7 +26,7 @@ def translate_output(transformer_outputs: torch.tensor) -> str:
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', type=str)
+    parser.add_argument('--path', type=str, required=True)
     args = parser.parse_args()
 
 
@@ -41,13 +44,20 @@ if __name__ == '__main__':
     data = provider.get_dataset('finetune', training=False)
     dataloader = dataset.DataLoader(data, batch_size=1)
 
-    for x, y in dataloader:
-        w = model(
-            x, 
-            y,
-            src_key_padding_mask=(x == w2i[PAD_TOKEN]),
-            tgt_key_padding_mask=(y == w2i[PAD_TOKEN]),
-        )
-        print(w)
-        print(translate_output(w))
-        a = input()
+    start_token = torch.tensor([[w2i[START_TOKEN]]])
+
+    with torch.no_grad():
+        for x, y in dataloader:
+            output_sequence = start_token
+            for _ in range(len(y[0])):
+                output = model(
+                    x, 
+                    output_sequence,
+                    src_key_padding_mask=(x == w2i[PAD_TOKEN]),
+                    tgt_key_padding_mask=None
+                )
+                last_token = torch.argmax(output[:, -1, :], dim=1, keepdim=True)
+                output_sequence = torch.cat((output_sequence, last_token), dim=1)
+            print(translate_output(output_sequence[0]))
+            print()
+            a = input()
