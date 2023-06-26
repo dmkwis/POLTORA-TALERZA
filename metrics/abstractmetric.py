41a7c5afb0a8e0358a66dd8b072e532a207fb574
dmkwis <dw418484@students.mimuw.edu.pl>
2023-06-26 22:06:28 +0200

    metrics init

---
+++ metrics/abstractmetric.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+class AbstractMetric(ABC):
+    def __init__(self, metric_name):
+        self.metric_name = metric_name
+
+    """
+    
+    """
+    @staticmethod
+    @abstractmethod
+    def compute(prompt_text: List[str], generated_text: List[List[str]]) -> float:
+        pass
