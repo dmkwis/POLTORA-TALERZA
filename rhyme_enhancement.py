842d80bd06cd6371e59da74a6e714079f2d4abb0
kar655 <karol123zag@gmail.com>
2023-06-26 20:49:55 +0200

    Use rhyme data structure for common code

--- rhyme_enhancement.py
+++ rhyme_enhancement.py
@@ -4,12 +4,45 @@ from copy import deepcopy
 from typing import List, Tuple
 
 
+class RhymeData:
+    def __init__(self, words: List[List[str]], idxs: Tuple[int, int], tokenizer):
+        self.words = deepcopy(words)
+        self.idxs = idxs
+        self.tokenizer = tokenizer
+
+        self.src = self.words[self.idxs[0]][-1]
+        self.tgt = self.words[self.idxs[1]][-1]
+
+        # Mask first word
+        self.words[self.idxs[0]][-1] = tokenizer.mask_token
+
+        # Restore lines and sentence
+        self.lines = [' '.join(self.words[i]) for i in range(4)]
+        self.sentence = tokenizer.sep_token.join(self.lines)
+
+        self.indexed_tokens = tokenizer.encode(self.sentence, add_special_tokens=True)
+
+    def prepare_tensors(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        segments_ids = []
+        mask_idxs = []
+        separators_found = 0
+        for i, token in enumerate(self.indexed_tokens):
+            segments_ids.append(separators_found)
+            if token == self.tokenizer.sep_token_id:
+                separators_found += 1
+            elif token == self.tokenizer.mask_token_id:
+                mask_idxs.append(i)
+
+        return mask_idxs, torch.tensor(segments_ids)
+
+
 class RhymeEnhancer:
     def __init__(self):
         self.tokenizer = torch.hub.load('huggingface/pytorch-transformers', 'tokenizer', 'bert-base-cased')
         self.bert = torch.hub.load('huggingface/pytorch-transformers', 'modelForMaskedLM', 'bert-base-cased')
 
-    def find_match(self, indexed_tokens: List[int], K: int, mask_idx: int, src_word: str, tgt_word: str, segments_tensors: torch.Tensor) -> Tuple[str, float]:
+    def find_match(self, indexed_tokens: List[int], K: int, mask_idx: int, src_word: str, tgt_word: str,
+                   segments_tensors: torch.Tensor) -> Tuple[str, float]:
         # TODO replace with actual rhyme metric
         def rhyme_length(a: str, b: str) -> float:
             print(f'Calculating rhyme length of word \'{a}\' and \'{b}\'')
@@ -18,7 +51,6 @@ class RhymeEnhancer:
         # src_word = self.tokenizer.convert_ids_to_tokens(indexed_tokens[mask_idx])
         rl_original = rhyme_length(src_word, tgt_word)
 
-        indexed_tokens[mask_idx] = self.tokenizer.mask_token_id
         tokens_tensor = torch.tensor([indexed_tokens])
         print(tokens_tensor)
         # predictions = self.bert(tokens_tensor, token_type_ids=segments_tensors).logits
@@ -42,53 +74,22 @@ class RhymeEnhancer:
         lines = sentence.split('\n')
         assert len(lines) == 4
         words = [line.split() for line in lines]
-        words_first = deepcopy(words)
-        # words_second = deepcopy(words)
 
-        src_first = words_first[0][-1]
-        tgt_first = words_first[2][-1]
-        words_first[0][-1] = self.tokenizer.mask_token
+        rhyme_data_first = RhymeData(words, (0, 2), self.tokenizer)
+        # rhyme_data_second = RhymeData(words, (2, 0), self.tokenizer)
 
-        # words_second[2][-1] = self.tokenizer.mask_token
-
-        lines_first = [' '.join(words_first[i]) for i in range(4)]
-        print('Lines changed', lines_first)
-        sentence_first = self.tokenizer.sep_token.join(lines_first)
-        print('Sentence changed', sentence_first)
-
-        # lines_second = [' '.join(words_second[i]) for i in range(4)]
-        # print('Lines changed', lines_second)
-        # sentence_second = '\n'.join(lines_second)
-        # print('Sentence changed', sentence_second)
-
-        # segment_ids = [[i] * len(line.split()) for i, line in enumerate(lines)]
-        # print(segment_ids)
-
-        indexed_tokens = self.tokenizer.encode(sentence_first, add_special_tokens=True)
+        indexed_tokens = self.tokenizer.encode(rhyme_data_first.sentence, add_special_tokens=True)
         print(indexed_tokens)
-
         # debug print
         print()
         for token in indexed_tokens:
             print(token, self.tokenizer.convert_ids_to_tokens(token))
         print()
 
-        segments_ids = []
-        mask_idxs = []
-        separators_found = 0
-        for i, token in enumerate(indexed_tokens):
-            segments_ids.append(separators_found)
-            if token == self.tokenizer.sep_token_id:
-                separators_found += 1
-            elif token == self.tokenizer.mask_token_id:
-                mask_idxs.append(i)
+        mask_idxs, segments_tensors = rhyme_data_first.prepare_tensors()
 
-        print(segments_ids)
-        print(mask_idxs)
-
-        segments_tensors = torch.tensor([segments_ids])
-
-        first_word, rl_first = self.find_match(indexed_tokens, K, mask_idxs[0], src_first, tgt_first, segments_tensors)
+        first_word, rl_first = self.find_match(indexed_tokens, K, mask_idxs[0], rhyme_data_first.src,
+                                               rhyme_data_first.tgt, segments_tensors)
         # self.find_match(indexed_tokens, K, separator_idxs[1] - 1, separator_idxs[0] - 1)
 
         return first_word
